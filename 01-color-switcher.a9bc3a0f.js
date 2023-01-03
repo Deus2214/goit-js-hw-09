f2574c319a08fc468b447d2b7d22e4bf3e508aaf
Deus2214 <110825559+Deus2214@users.noreply.github.com>
2023-01-03 18:26:57 +0000

    Deploying to gh-pages from @ Deus2214/goit-js-hw-09@ec8e19a1869693fe143169e1ebe7a26830c599e2 🚀

--- 01-color-switcher.a9bc3a0f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){r=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.currentTarget.disabled=!0,e.removeAttribute("disabled")})),e.addEventListener("click",(function(e){clearInterval(r),e.currentTarget.disabled=!0,t.removeAttribute("disabled")})),e.disabled=!0;var r=null}();
-//# sourceMappingURL=01-color-switcher.a9bc3a0f.js.map
